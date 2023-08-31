c31380ae703ebe258b20c4f23cae2df54a4b6348
Sharun <sharunksplus@gmail.com>
2023-08-31 15:46:29 -0400

    remove output line

--- .github/scripts/update_readmes.py
+++ .github/scripts/update_readmes.py
@@ -18,8 +18,5 @@ def main():
     offseason_listings = util.filterOffSeason(listings)
     util.embedTable(offseason_listings, "README-Off-Season.md", offSeason=True)
 
-    util.setOutput("commit_message", "Updating READMEs at " + datetime.now().strftime("%B %d, %Y %H:%M:%S"))
-
-
 if __name__ == "__main__":
     main()
